3cd93b1fbaf98550568246a38badb942f30bcf21
korogodiniv@gmail.com <korogodiniv@gmail.com@5500cb53-4492-edb6-91cc-8742f66b184c>
2012-06-13 19:53:33 +0000

    Выкидываем лишнее

--- func/solve/fsolve_Kepler.m
+++ func/solve/fsolve_Kepler.m
@@ -11,21 +11,7 @@ i = Xs(4);
 e = Xs(5);
 p = Xs(6);
 
-Xsolve(1) = p / (1 + e*cos(theta));
-Xsolve(2) = theta + omega_p;
-Xsolve(3) = Omega;
-Xsolve(4) = i;
-Xsolve(5) = sqrt(mu_earth/p) * e * sin(theta);
-if Xsolve(1) ~= 0
-    Xsolve(6) = sqrt(mu_earth/p) * (1 + e*cos(theta)) / Xsolve(1);
-else
-    Xsolve(6) = 0;
-end
-
-[x y z] = ...
-        get_vector_XYZ(Xsolve(1), Xsolve(3), Xsolve(4), Xsolve(2));
-
-[Vx, Vy, Vz] = get_vector_VxVyVz( Xsolve(1), Xsolve(5), Xsolve(3), Xsolve(4), Xsolve(2), Xsolve(6), x, y, z);
+[x, y, z, Vx, Vy, Vz] = get_vector_XV( e, p, theta, omega_p, Omega, i);
 
 ErrX = Xizm - x;
 ErrY = Yizm - y;
